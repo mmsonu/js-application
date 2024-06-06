34a22f8af6cb055d9dd8c8633fbec7ed1eb53849
mmsonu <shaikatheeq050@gmail.com>
2024-06-06 12:54:30 +0530

    node.js files

---
+++ Index.js
@@ -0,0 +1,1 @@
+Often used as an entry point for modules, it can be the main file for the application..
---
+++ server.js
@@ -0,0 +1,1 @@
+These are the main entry points of the application where the server is configured and started.
